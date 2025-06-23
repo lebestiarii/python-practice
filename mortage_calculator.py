a6ed554508fcbd57514b60b70c9b41266b4e5bd7
lebestiarii <lebestiarii@pm.me>
2025-06-22 20:49:30 -0700

    Cleaned up code and comments.

--- mortage_calculator.py
+++ mortage_calculator.py
@@ -11,11 +11,12 @@ def greet_user(first_name, last_name):
 
 def read_number(prompt, min, max):
     """
-
+    Prompts the user for an integer value to determine if the value is
+    between the minium and maximum values
     :param prompt: A string that will be printed
-    :param min: The minimum value expected
-    :param max: The maximum value expected
-    :return: The input value entered by the user
+    :param min: The minimum value expected for the value
+    :param max: The maximum value expected for the value
+    :return: The input integer value entered by the user
     """
     while True:
         value = int(input(prompt))
@@ -26,11 +27,11 @@ def read_number(prompt, min, max):
 
 def calculate_mortgage(principal, annual_interest, years):
     """
-    Calculate Mortgage value
+    Calculate Mortgage value using the principal, annual_interest, and term years.
     :param principal: A number between 1000 and 1_000_000
     :param annual_interest: a number between 1 and 30
     :param years: A number between 1 and 30
-    :return:
+    :return: Mortgage monthly payment value
     """
     monthly_interest = annual_interest / PERCENT / MONTHS_IN_YEARS
     number_of_payments = years * MONTHS_IN_YEARS
@@ -45,18 +46,34 @@ def calculate_balance(principal, annual_interest, years, number_of_payments_made
     total_number_of_payments = years * MONTHS_IN_YEARS
 
     # Correct formula for remaining balance
-    numerator = math.pow(1 + monthly_interest, total_number_of_payments) - math.pow(1 + monthly_interest, number_of_payments_made)
+    numerator = (math.pow(1 + monthly_interest, total_number_of_payments)
+                 - math.pow(1 + monthly_interest, number_of_payments_made))
     denominator = math.pow(1 + monthly_interest, total_number_of_payments) - 1
 
     return principal * (numerator / denominator)
 
 def print_mortgage(principal, annual_interest, years):
+    """
+    Prints the calculated Mortgage value using the principal, annual_interest, and years.
+    :param principal:
+    :param annual_interest:
+    :param years:
+    :return:
+    """
     mortgage = calculate_mortgage(principal, annual_interest, years)
-    print(f"MORTGAGE\n"
-          f"------------------------------------------"
+    print(f"\nMORTGAGE\n"
+          f"------------------------------------------\n"
           f"Monthly Payments: ${mortgage:.2f}\n")
 
 def print_payment_schedule(principal, annual_interest, years):
+    """
+    Prints the payment schedule per month by calculating your mortgage payments
+    value using the principal, annual_interest, and years.
+    :param principal:
+    :param annual_interest:
+    :param years:
+    :return:
+    """
     months = years * MONTHS_IN_YEARS
     print(f"PAYMENT SCHEDULE\n"
           f"------------------------------------------")
@@ -68,10 +85,12 @@ def main():
     # Greet the user
     greet_user("John", "Smith")
 
+    # Get user inputs
     principal = read_number("Principal: ",1000,1_000_000)
     annual_interest_rate = read_number("Annual Interest Rate: ",1,30)
     years = read_number("Years: ",1,30)
 
+    # Calculate and display the mortgage information based on the user inputs.
     print_mortgage(principal, annual_interest_rate, years)
     print_payment_schedule(principal, annual_interest_rate, years)
 
